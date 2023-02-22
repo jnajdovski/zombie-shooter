458890580d9303e54a1cd0de645b1bc8d542f77c
Jovan <jovan.najdovski@outlook.com>
2023-02-22 19:15:12 +0100

    fix: added game scene to the game

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import {
     Game
 } from 'phaser'
 import config from './config'
-import Boot from './scenes/Boot'
-import Menu from './scenes/Menu'
+import Boot from './scenes/BootScene'
+import Menu from './scenes/MenuScene'
+import GameScene from './scenes/GameScene'
 
 const game = new Game(config)
 
 game.scene.add('boot', new Boot({}))
 game.scene.add('menu', new Menu({}))
+game.scene.add('game', new GameScene({}))
 game.scene.start('boot')
