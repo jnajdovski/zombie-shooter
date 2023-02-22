f6dbedd140d9c5c174422cb34018761a7ef9d725
Jovan <jovan.najdovski@outlook.com>
2023-02-22 19:14:00 +0100

    fix: added croshair class for pointer

---
+++ src/components/Crosshair.ts
@@ -0,0 +1,14 @@
+import { GameObjects, Scene } from "phaser";
+
+export default class Crosshair extends GameObjects.Sprite {
+    constructor(scene: Scene) {
+        super(scene, scene.cameras.main.width / 2, scene.cameras.main.height / 2, 'crosshair')
+        this.setOrigin(0.5, 0.5)
+        this.setScale(.1)
+        scene.children.add(this)
+    }
+
+    pointerMove(pointer: PointerEvent) {
+        this.setPosition(pointer.x, pointer.y)
+    }
+}
