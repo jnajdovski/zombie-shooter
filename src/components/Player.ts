1e77b6330301b0ae0c581e24e7c0564140b9c650
Jovan <jovan.najdovski@outlook.com>
2023-02-23 01:17:44 +0100

    fix: implement player to shoot bullets

--- src/components/Player.ts
+++ src/components/Player.ts
@@ -1,88 +1,96 @@
 import { GameObjects, Input, Scene } from "phaser";
-import { keyDown } from "../helpers/keyboardMovement";
+import Pool, { PoolObject } from "../helpers/Pool";
+import Bullet from "./Bullet";
 
 export default class Player extends GameObjects.Sprite {
-
-    playerAnimations: { name: string, endFrame: number }[] = [{ name: 'player_idle', endFrame: 19 }, { name: 'player_move', endFrame: 19 }, { name: 'player_shoot', endFrame: 2 }];
-    speed: number = 8
-    keys: { W: Input.Keyboard.Key, A: Input.Keyboard.Key, S: Input.Keyboard.Key, D: Input.Keyboard.Key }
-    movingLeft: boolean = false
-    movingRight: boolean = false
-    movingDown: boolean = false
-    movingUp: boolean = false
-    playerShoot: boolean = false
-
-    keyA: Input.Keyboard.Key
-    keyS: Input.Keyboard.Key
-    keyD: Input.Keyboard.Key
-    keyW: Input.Keyboard.Key
+    private playerAnimations: { name: string, endFrame: number }[] = [{ name: 'player_idle', endFrame: 19 }, { name: 'player_move', endFrame: 19 }, { name: 'player_shoot', endFrame: 2 }];
+    private speed: number = 8
+    private playerShoot: boolean = false
+    private keyA: Input.Keyboard.Key
+    private keyS: Input.Keyboard.Key
+    private keyD: Input.Keyboard.Key
+    private keyW: Input.Keyboard.Key
+    private waitForNextBullet = false
+    private bulletsPool: Pool
 
     constructor(scene: Scene, x: number, y: number) {
         super(scene, x, y, 'player_idle', 0)
-        this.setOrigin(0.5, 0.5)
+        this.setOrigin(0.5, 0.7)
         this.setScale(0.8)
         this.createAnimations()
         this.idle()
+        this.setPlayerEvents()
+
+        this.bulletsPool = new Pool(() => this.createBullets(), (obj) => this.resetBullets(obj), 100)
+
         scene.children.add(this)
-
-        this.keys = {
-            W: scene.input.keyboard.addKey('W'),
-            A: scene.input.keyboard.addKey('A'),
-            S: scene.input.keyboard.addKey('S'),
-            D: scene.input.keyboard.addKey('D')
-        }
-
-        // window.addEventListener('keydown', (event) => {
-        //     if (keyDown(event) === 'left') this.movingLeft = true
-        //     if (keyDown(event) === 'right') this.movingRight = true
-        //     if (keyDown(event) === 'down') this.movingDown = true
-        //     if (keyDown(event) === 'up') this.movingUp = true
-        // });
-
-        // window.addEventListener('keyup', (event) => {
-        //     if (keyDown(event) === 'left') this.movingLeft = false
-        //     if (keyDown(event) === 'right') this.movingRight = false
-        //     if (keyDown(event) === 'down') this.movingDown = false
-        //     if (keyDown(event) === 'up') this.movingUp = false
-        // });
-
-        this.keyW = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
-        this.keyA = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
-        this.keyS = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
-        this.keyD = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
-
-        // scene.input.on('keydown', () => {
-        //     console.log('key down');
-
-        // })
-
-        scene.input.on('pointerdown', () => {
-            this.playerShoot = true
-            this.shoot()
-        })
-        scene.input.on('pointerup', () => {
-            this.playerShoot = false
-            this.idle()
-        })
     }
 
-    idle() {
+    private setPlayerEvents() {
+        this.keyW = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+        this.keyA = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+        this.keyS = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+        this.keyD = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+
+        this.scene.input.on('pointerdown', (pointer: PointerEvent) => {
+            if (pointer.button === 0) {
+                this.playerShoot = true
+                this.shoot()
+            }
+        })
+
+        this.scene.input.on('pointerup', () => {
+            this.playerShoot = false
+            this.waitForNextBullet = false
+            this.idle()
+        })
+
+        this.setInteractive()
+        this.scene.input.on('pointermove', (pointer) => this.rotatePlayer(pointer))
+    }
+
+    public rotatePlayer(pointer: PointerEvent) {
+        this.rotation = Phaser.Math.Angle.BetweenPoints(this, pointer)
+    }
+
+    private createBullets() {
+        return new Bullet(this.scene, this.x, this.y)
+    }
+
+    private resetBullets(obj: PoolObject) {
+        obj.data.hide()
+    }
+
+    private idle() {
         if (!this.playerShoot) {
             this.stop()
             this.play('player_idle')
         }
     }
 
-    move() {
+    private move() {
         if (!this.playerShoot) {
             this.stop()
             this.play('player_move')
         }
     }
 
-    shoot() {
-        this.stop()
+    private shoot() {
         this.play('player_shoot')
+        if (!this.waitForNextBullet) {
+            this.fireBullet()
+        }
+    }
+
+    private fireBullet() {
+        this.waitForNextBullet = true
+        const bullet = this.bulletsPool.getFree()
+        bullet.data.show(this.x, this.y)
+        bullet.data.rotation = this.rotation
+
+        setTimeout(() => {
+            this.waitForNextBullet = false
+        }, 150);
     }
 
     private moveLeft() {
@@ -117,8 +125,15 @@ export default class Player extends GameObjects.Sprite {
         }
     }
 
-    rotatePlayer(pointer: PointerEvent) {
-        this.rotation = Phaser.Math.Angle.BetweenPoints(this, pointer)
+    private createAnimations() {
+        for (let anim of this.playerAnimations) {
+            this.scene.anims.create({
+                key: anim.name,
+                frameRate: 40,
+                frames: this.scene.anims.generateFrameNumbers(anim.name, { start: 0, end: anim.endFrame }),
+                repeat: anim.name === 'player_shoot' ? 1 : -1,
+            })
+        }
     }
 
     update() {
@@ -127,16 +142,19 @@ export default class Player extends GameObjects.Sprite {
         if (this.keyS.isDown) this.moveDown()
         if (this.keyW.isDown) this.moveUp()
         if (this.keyW.isUp && this.keyA.isUp && this.keyS.isUp && this.keyD.isUp) this.idle()
+
+        this.updateBullets()
     }
 
-    private createAnimations() {
-        for (let anim of this.playerAnimations) {
-            this.scene.anims.create({
-                key: anim.name,
-                frameRate: 40,
-                frames: this.scene.anims.generateFrameNumbers(anim.name, { start: 0, end: anim.endFrame }),
-                repeat: -1
-            })
-        }
+    updateBullets() {
+        this.bulletsPool.pool.forEach((obj) => {
+            if (!obj.free) {
+                if (obj.data.x > 0 && obj.data.x < this.scene.cameras.main.width
+                    && obj.data.y > 0 && obj.data.y < this.scene.cameras.main.height) {
+                    obj.data.update()
+                }
+                else this.bulletsPool.release(obj)
+            }
+        })
     }
 }
