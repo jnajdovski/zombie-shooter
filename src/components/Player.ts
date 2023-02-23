3163a366ca6954b9360718bfd7d6601722cf2549
Jovan <jovan.najdovski@outlook.com>
2023-02-23 22:59:30 +0100

    fix: added logic for player health and kills

--- src/components/Player.ts
+++ src/components/Player.ts
@@ -3,7 +3,7 @@ import Pool, { PoolObject } from "../helpers/Pool";
 import Bullet from "./Bullet";
 
 export default class Player extends GameObjects.Sprite {
-    private playerAnimations: { name: string, endFrame: number }[] = [{ name: 'player_idle', endFrame: 19 }, { name: 'player_move', endFrame: 19 }, { name: 'player_shoot', endFrame: 2 }];
+    private playerAnimations: string[] = ['player_idle', 'player_move', 'player_shoot'];
     private speed: number = 8
     private playerShoot: boolean = false
     private keyA: Input.Keyboard.Key
@@ -11,7 +11,9 @@ export default class Player extends GameObjects.Sprite {
     private keyD: Input.Keyboard.Key
     private keyW: Input.Keyboard.Key
     private waitForNextBullet = false
-    private bulletsPool: Pool
+    public bulletsPool: Pool
+    private kills: number = 0
+    private health: number = 200
 
     constructor(scene: Scene, x: number, y: number) {
         super(scene, x, y, 'player_idle', 0)
@@ -22,8 +24,15 @@ export default class Player extends GameObjects.Sprite {
         this.setPlayerEvents()
 
         this.bulletsPool = new Pool(() => this.createBullets(), (obj) => this.resetBullets(obj), 100)
-
+        this.scene.physics.add.existing(this)
         scene.children.add(this)
+
+        //@ts-ignore
+        this.body.offset.set(100, 100)
+        //@ts-ignore
+        this.body.width = 100
+        //@ts-ignore
+        this.body.height = 100
     }
 
     private setPlayerEvents() {
@@ -49,8 +58,30 @@ export default class Player extends GameObjects.Sprite {
         this.scene.input.on('pointermove', (pointer) => this.rotatePlayer(pointer))
     }
 
+    public updateHealth(): void {
+        this.health -= 5
+    }
+
+    public getHealth(): number {
+        return this.health
+    }
+
+
+    public updateKills() {
+        this.kills += 1
+    }
+
+    public getKills() {
+        return this.kills
+    }
+
+
     public rotatePlayer(pointer: PointerEvent) {
         this.rotation = Phaser.Math.Angle.BetweenPoints(this, pointer)
+    }
+
+    public getPosition(): { x: number, y: number } {
+        return { x: this.x, y: this.y }
     }
 
     private createBullets() {
@@ -59,6 +90,7 @@ export default class Player extends GameObjects.Sprite {
 
     private resetBullets(obj: PoolObject) {
         obj.data.hide()
+        obj.free = true
     }
 
     private idle() {
@@ -87,6 +119,7 @@ export default class Player extends GameObjects.Sprite {
         const bullet = this.bulletsPool.getFree()
         bullet.data.show(this.x, this.y)
         bullet.data.rotation = this.rotation
+        bullet.free = false
 
         setTimeout(() => {
             this.waitForNextBullet = false
@@ -128,10 +161,10 @@ export default class Player extends GameObjects.Sprite {
     private createAnimations() {
         for (let anim of this.playerAnimations) {
             this.scene.anims.create({
-                key: anim.name,
+                key: anim,
                 frameRate: 40,
-                frames: this.scene.anims.generateFrameNumbers(anim.name, { start: 0, end: anim.endFrame }),
-                repeat: anim.name === 'player_shoot' ? 1 : -1,
+                frames: this.scene.anims.generateFrameNumbers(anim, { start: 0 }),
+                repeat: anim === 'player_shoot' ? 1 : -1,
             })
         }
     }
@@ -144,6 +177,9 @@ export default class Player extends GameObjects.Sprite {
         if (this.keyW.isUp && this.keyA.isUp && this.keyS.isUp && this.keyD.isUp) this.idle()
 
         this.updateBullets()
+
+        this.body.velocity.x = 0
+        this.body.velocity.y = 0
     }
 
     updateBullets() {
