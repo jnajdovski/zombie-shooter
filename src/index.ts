1b93524b171d892e39213842c4cb437464646097
Jovan <jovan.najdovski@outlook.com>
2023-02-24 03:55:32 +0100

    fix: added game over scene

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import config from './config'
 import Boot from './scenes/BootScene'
 import Menu from './scenes/MenuScene'
 import GameScene from './scenes/GameScene'
+import GameOverScene from './scenes/GameOverScene'
 
 const game = new Game(config)
 
 game.scene.add('boot', new Boot({}))
 game.scene.add('menu', new Menu({}))
 game.scene.add('game', new GameScene({}))
+game.scene.add('game_over', new GameOverScene({}))
 game.scene.start('boot')
---
+++ src/scenes/GameOverScene.ts
@@ -0,0 +1,47 @@
+import { GameObjects, Scene } from "phaser";
+import TextButton from "../components/TextButton";
+
+export default class GameOverScene extends Scene {
+    private gameOverText: GameObjects.Text
+    private backToMenu: TextButton
+    private background: GameObjects.Image
+    private killsText: GameObjects.Text
+
+    create({ kills }) {
+
+        this.background = new GameObjects.Image(this, 0, 0, 'background')
+        this.background.setOrigin(0, 0)
+        this.background.setDisplaySize(this.cameras.main.width, this.cameras.main.height)
+        this.background.setTint(0x962c0c)
+        this.children.add(this.background)
+
+        this.gameOverText = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2 - 50, 'GAME OVER', {
+            fontFamily: 'Quicksand',
+            fontSize: '68px',
+            color: '#fff'
+        })
+        this.gameOverText.setOrigin(0.5)
+        this.children.add(this.gameOverText)
+
+        this.killsText = this.add.text(this.gameOverText.x, this.gameOverText.y + 50, `KILLS: ${kills}`, {
+            fontFamily: 'Quicksand',
+            fontSize: '50px',
+            color: '#fff'
+        })
+        this.killsText.setOrigin(0.5)
+        this.children.add(this.killsText)
+
+        this.backToMenu = new TextButton(this, this.cameras.main.width / 2, this.cameras.main.height - 50, 'BACK TO MENU', {
+            fontFamily: 'Quicksand',
+            fontSize: '42px',
+            color: '#fff'
+        })
+
+        this.backToMenu.enable()
+
+        this.children.add(this.backToMenu)
+        this.backToMenu.on('click', () => {
+            this.scene.start('menu')
+        })
+    }
+}
